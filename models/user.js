f448660b1c7f3fb4a09b988799ed429d00881425
ikun2019 <0215.miyatto@gmail.com>
2022-08-28 18:14:46 +0900

    Userモデルの作成

---
+++ models/user.js
@@ -0,0 +1,52 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "名前は必須です。"
+        }
+      }
+    },
+    mail: {
+      type: DataTypes.STRING,
+      validate: {
+        msg: "メールアドレスは必須です"
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        msg: "パスワードは必須です"
+      }
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: {msg: "整数を入力してください。"},
+        min: {
+          args: [0],
+          msg: "0以上の値が必要です。"
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
